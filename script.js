fe6f09a6f81fdb522946905c5f3b148f4d3c9e95
William Luck <luck.william.h@gmail.com>
2022-04-13 17:28:52 -0500

    connect buttons with corresponding percentage

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-// const h2 = document.createElement('h2');
-// h2.textContent = "this content added by JavaScript";
+const skill1PercentageNode = document.getElementById('skill-1-percentage')
+const skill2PercentageNode = document.getElementById('skill-2-percentage')
+const skill3PercentageNode = document.getElementById('skill-3-percentage')
+const skill4PercentageNode = document.getElementById('skill-4-percentage')
+let percentage1 = 0;
+let percentage2 = 0;
+let percentage3 = 0;
+let percentage4 = 0;
 
+// add event listener to each task button, adds logic to each set of two buttons
 const taskButtons = document.querySelectorAll('.task-button');
-
-
 for (const node of taskButtons) {
     node.addEventListener('click', e => {
-        console.log(e.target.id)
-        // if ((e.target.id === 'task-name-button1') || (e.target.id === 'task-name-button2')) {
-        //     console.log("corresponds with skill 1")
-        // } else {
-        //     console.log("corresponds with other skill")
-        // }
-    
+        if ((e.target.id === 'task-name-button1') || (e.target.id === 'task-name-button2')) {
+            console.log("corresponds with skill 1")
+            percentage1 = updatePercentage(skill1PercentageNode, percentage1);
+        } else if ((e.target.id === 'task-name-button3') || (e.target.id === 'task-name-button4')){
+            console.log("corresponds with skill 2")
+            percentage2 = updatePercentage(skill2PercentageNode,percentage2);
+        } else if ((e.target.id === 'task-name-button5') || (e.target.id === 'task-name-button6')) {
+            console.log("corresponds with skill 3")
+            percentage3 = updatePercentage(skill3PercentageNode, percentage3);
+        } else if ((e.target.id === 'task-name-button7') || (e.target.id === 'task-name-button8')) {
+            console.log("corresponds with skill 4")
+            percentage4 = updatePercentage(skill4PercentageNode,percentage4);
+        }
 })}
 
 
-
-
-/*
-
-<p id="skill-1-percentage">Skill 1: 0%</p>
-<p id="skill-2-percentage">Skill 2: 0%</p>
-<p id="skill-3-percentage">Skill 3: 0%</p>
-<p id="skill-4-percentage">Skill 4: 0%</p>
-
-*/
-
-const skill1PercentageNode = document.getElementById('skill-1-percentage')
-const skill2PercentageNode = document.getElementById('skill-2-percentage')
-const skill3PercentageNode = document.getElementById('skill-4-percentage')
-const skill4PercentageNode = document.getElementById('skill-5-percentage')
+// updates corresponding percentage
+function updatePercentage(node, percentage) {
+    percentage += 20;
+    node.textContent = `Progress: ${percentage}%`;
+    return percentage;
+}
