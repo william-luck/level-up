f5a40330160074151bde8b531d52eb4d81d74780
William Luck <luck.william.h@gmail.com>
2022-05-09 15:47:53 -0500

    Reordered functions and cleaned up code

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const skill1PercentageNode = document.getElementById('self-comp-percentage')
-const skill2PercentageNode = document.getElementById('pers-percentage')
-const skill3PercentageNode = document.getElementById('gh-percentage')
-const skill4PercentageNode = document.getElementById('supp-percentage')
+const stickFigureContainer = document.getElementById('stick-figure-container')
 
-const skill1ProgressBar = document.getElementById('skill-1-progress-bar')
-const skill2ProgressBar = document.getElementById('skill-2-progress-bar')
-const skill3ProgressBar = document.getElementById('skill-3-progress-bar')
-const skill4ProgressBar = document.getElementById('skill-4-progress-bar')
+const selfCompPercentageNode = document.getElementById('self-comp-percentage')
+const persPercentageNode = document.getElementById('pers-percentage')
+const ghPercentageNode = document.getElementById('gh-percentage')
+const suppPercentageNode = document.getElementById('supp-percentage')
 
-let percentage1 = 0;
-let percentage2 = 0;
-let percentage3 = 0;
-let percentage4 = 0;
+const selfCompProgressBar = document.getElementById('self-comp-progress-bar')
+const persProgressBar = document.getElementById('pers-progress-bar')
+const ghProgressBar = document.getElementById('gh-3-progress-bar')
+const suppProgressBar = document.getElementById('supp-4-progress-bar')
 
-let axeRotation = 0
+let selfCompPercentage = 0;
+let persPercentage = 0;
+let ghPercentage = 0;
+let suppPercentage = 0;
 
 const rewardsTile = document.getElementById('reward-announcement')
 const rewardsList = document.getElementById('reward-list')
@@ -21,155 +21,15 @@ const rewardsList = document.getElementById('reward-list')
 const taskLog = document.getElementById('task-log')
 const completedTasksContainer = document.getElementById('completed-task-container') 
 
-const stickFigureContainer = document.getElementById('stick-figure-container')
+
 
 let tasksCompleted = 0
 
 const key = config.accessKey;
 
+let axeRotation = 0
 
 
-
-// add event listener to each task button, adds logic to each set of two buttons
-const taskButtons = document.querySelectorAll('.task-button');
-for (const node of taskButtons) {
-    node.addEventListener('click', e => {
-        console.log(e)
-        if ((e.target.id === 'self-comp-1') || (e.target.id === 'self-comp-2')) {
-            if (percentage1 < 100) {
-                percentage1 = updatePercentage(skill1PercentageNode, percentage1, ' - Self Compassion');
-                updateProgressBar(skill1ProgressBar, percentage1);
-                addTaskCompleted(e);
-            }
-        } else if ((e.target.id === 'pers-1') || (e.target.id === 'pers-2')){
-            if (percentage2 < 100) {
-                percentage2 = updatePercentage(skill2PercentageNode,percentage2, ' - Persistence');
-                updateProgressBar(skill2ProgressBar, percentage2);
-                addTaskCompleted(e)
-            }
-        } else if ((e.target.id === 'gh-1') || (e.target.id === 'gh-2')) {
-            if (percentage3 < 100) {
-                percentage3 = updatePercentage(skill3PercentageNode, percentage3, ' - Good Habits');
-                updateProgressBar(skill3ProgressBar, percentage3);
-                addTaskCompleted(e)
-            }
-        } else if ((e.target.id === 'supp-1') || (e.target.id === 'supp-2')) {
-            if (percentage4 < 100) {
-                percentage4 = updatePercentage(skill4PercentageNode,percentage4, ' - Support');
-                updateProgressBar(skill4ProgressBar, percentage4);
-                addTaskCompleted(e)
-            }
-        }
-})}
-
-
-
-
-
-
-// updates corresponding percentage
-function updatePercentage(node, percentage, skillString) {
-    percentage += 20;
-    node.textContent = `${percentage}%${skillString}`;
-
-    if (percentage === 100) {
-        displayAward(node)
-    }
-
-    return percentage;
-}
-
-// updates corresponding progress bar
-function updateProgressBar(progressBar, percentage) {
-    progressBar.src = `images/${percentage}-percent.png`;
-}
-
-function addTaskCompleted(e) {
-
-    // adds name of completed task to tag log
-    const completedTask = document.createElement('p')
-    completedTask.textContent = e.target.textContent;
-    completedTasksContainer.appendChild(completedTask)
-
-    // creates form underneath the completed task
-    const inputForm = document.createElement('form')
-    inputForm.id = 'input-form'
-    completedTask.appendChild(inputForm)
-
-    // Adds comment bar
-    const inputText = document.createElement('input')
-    inputText.type = 'text'
-    inputText.value = ''
-    inputText.placeholder = 'Add comment..'
-    inputText.id = 'comment'
-    inputForm.appendChild(inputText)
-
-    // Adds submit button 
-    const submitButton = document.createElement('input')
-    submitButton.type = 'submit'
-    submitButton.name = 'submit'
-    submitButton.value = 'Add'
-    inputForm.appendChild(submitButton)
-
-    // Adds comment to task completed upon submission of form
-    inputForm.addEventListener('submit', addCommentToTask)      
-    
-    tasksCompleted+=1
-    if (tasksCompleted === 1) {
-        const hideButton = createHideButton()
-        let hidden = false
-        hideButton.addEventListener('click', e => {
-            if (hidden === false) {
-                hideButton.textContent = 'Show'
-                hidden = true
-                completedTasksContainer.style.display ='none'
-            } else if (hidden === true) {
-                hideButton.textContent = "Hide"
-                hidden = false
-                completedTasksContainer.style.display=''
-            }
-            
-        })
-    }
-    
-
-}
-
-
-
-
-function displayAward(node) {
-    rewardsTile.textContent = "Earned rewards:"
-
-    if (node.id === 'self-comp-percentage') {
-        addHeart();
-    } else if (node.id === 'pers-percentage') {
-        addAxe();
-        window.addEventListener('keydown', rotateAxe)
-    } else if (node.id === 'gh-percentage') {
-        addShield();
-    } else if (node.id === 'supp-percentage') {
-        addFollowers();
-    }
-}
-
-// rotates by left and right arrows, only available after axe appears
-function rotateAxe(e) {
-    const axeNode = document.getElementById('axe')
-
-    if (e.key === 'ArrowLeft') {
-        axeRotation-=45
-    } else if (e.key === 'ArrowRight') {
-        axeRotation+=45
-    }
-
-    axeNode.style.transform = `rotate(${axeRotation}deg)`
-    
-
-
-    
-
-}
 
 // gets random smiling face from API to display on avatar
 fetch('https://emoji-api.com/emojis?search=smiling&access_key=' + key)
@@ -192,6 +52,165 @@ function addRandomFace(emoji) {
     stickFigureContainer.appendChild(randomFace)
 }
 
+
+
+
+// add event listener to each task button, adds logic to each set of two buttons
+const taskButtons = document.querySelectorAll('.task-button');
+for (const node of taskButtons) {
+    node.addEventListener('click', e => {
+        if ((e.target.id === 'self-comp-1') || (e.target.id === 'self-comp-2')) {
+            if (selfCompPercentage < 100) {
+                selfCompPercentage = updatePercentage(selfCompPercentageNode, selfCompPercentage, ' - Self Compassion');
+                updateProgressBar(selfCompProgressBar, selfCompPercentage);
+                addTaskCompleted(e);
+            }
+        } else if ((e.target.id === 'pers-1') || (e.target.id === 'pers-2')){
+            if (persPercentage < 100) {
+                persPercentage = updatePercentage(persPercentageNode,persPercentage, ' - Persistence');
+                updateProgressBar(persProgressBar, persPercentage);
+                addTaskCompleted(e)
+            }
+        } else if ((e.target.id === 'gh-1') || (e.target.id === 'gh-2')) {
+            if (ghPercentage < 100) {
+                ghPercentage = updatePercentage(ghPercentageNode, ghPercentage, ' - Good Habits');
+                updateProgressBar(ghProgressBar, ghPercentage);
+                addTaskCompleted(e)
+            }
+        } else if ((e.target.id === 'supp-1') || (e.target.id === 'supp-2')) {
+            if (suppPercentage < 100) {
+                suppPercentage = updatePercentage(suppPercentageNode,suppPercentage, ' - Support');
+                updateProgressBar(suppProgressBar, suppPercentage);
+                addTaskCompleted(e)
+            }
+        }
+})}
+
+
+
+
+
+
+// updates corresponding percentage
+function updatePercentage(node, percentage, skillString) {
+    percentage += 20;
+    node.textContent = `${percentage}%${skillString}`;
+
+    // triggers once percentage equals 100
+    if (percentage === 100) {
+        displayAward(node)
+    }
+
+    return percentage;
+}
+
+// updates corresponding progress bar
+function updateProgressBar(progressBar, percentage) {
+    progressBar.src = `images/${percentage}-percent.png`;
+}
+
+function addTaskCompleted(e) {
+
+    // adds name of completed task to tag log
+    const completedTask = document.createElement('p')
+    completedTask.textContent = e.target.textContent;
+    completedTasksContainer.appendChild(completedTask)
+
+    // creates form next to the completed task
+    const inputForm = document.createElement('form')
+    inputForm.id = 'input-form'
+    completedTask.appendChild(inputForm)
+
+    // Adds comment bar
+    const inputText = document.createElement('input')
+    inputText.type = 'text'
+    inputText.value = ''
+    inputText.placeholder = 'Add comment..'
+    inputText.id = 'comment'
+    inputForm.appendChild(inputText)
+
+    // Adds submit button 
+    const submitButton = document.createElement('input')
+    submitButton.type = 'submit'
+    submitButton.name = 'submit'
+    submitButton.value = 'Add'
+    inputForm.appendChild(submitButton)
+
+    // Adds comment to task completed upon submission of form
+    inputForm.addEventListener('submit', addCommentToTask)      
+    
+    // Adds show/hide button only on first task completed
+    tasksCompleted+=1
+    if (tasksCompleted === 1) {
+        const hideButton = createHideButton()
+        let hidden = false
+        hideButton.addEventListener('click', e => {
+            if (hidden === false) {
+                hideButton.textContent = 'Show'
+                hidden = true
+                completedTasksContainer.style.display ='none'
+            } else if (hidden === true) {
+                hideButton.textContent = "Hide"
+                hidden = false
+                completedTasksContainer.style.display=''
+            }
+            
+        })
+    }
+}
+
+//Removes form bar, adds comment to task. Callback function for submit event listener. 
+function addCommentToTask(e) {
+    e.preventDefault();
+
+    const comment = e.target.comment.value
+    const completedTask = e.target.parentNode  
+
+    e.target.remove()
+
+    const commentNode = document.createElement('ul')
+    const commentText = document.createElement('li')
+    commentNode.appendChild(commentText)
+
+    commentText.textContent = `Comment: ${comment}`
+    
+    completedTask.appendChild(commentNode)
+  }  
+
+
+  function createHideButton() {
+    const hideButton = document.createElement('button')
+    hideButton.textContent = 'Hide'
+
+    completedTasksContainer.parentNode.insertBefore(hideButton, completedTasksContainer)
+
+    return hideButton
+  }
+
+
+
+
+
+function displayAward(node) {
+    rewardsTile.textContent = "Earned rewards:"
+
+    if (node.id === 'self-comp-percentage') {
+        addHeart();
+    } else if (node.id === 'pers-percentage') {
+        addAxe();
+        window.addEventListener('keydown', rotateAxe)
+    } else if (node.id === 'gh-percentage') {
+        addShield();
+    } else if (node.id === 'supp-percentage') {
+        addFollowers();
+    }
+}
+
+// rotates by left and right arrows, only available after axe appears
+
+
+
+
 // Has find array iteration
 function addHeart() {
     fetch('https://emoji-api.com/emojis?search=heart&access_key=' + key)
@@ -209,9 +228,9 @@ function addHeart() {
         heartNode.style.fontSize = '26px'
         stickFigureContainer.appendChild(heartNode)
 
-        const reward1 = document.createElement('p');
-        reward1.textContent = `Earned a heart! ${heartEmoji}`
-        rewardsList.appendChild(reward1);
+        const rewardText = document.createElement('p');
+        rewardText.textContent = `Earned a heart! ${heartEmoji}`
+        rewardsList.appendChild(rewardText);
 
         
     })
@@ -232,12 +251,23 @@ function addAxe() {
         axeNode.style.transform = `rotate(${axeRotation}deg)`
         stickFigureContainer.appendChild(axeNode)
 
-        // axeNode.style.transform = 'rotate(90deg)'
 
-        const reward2 = document.createElement('p');
-        reward2.textContent = `Earned axe! (You can rotate the axe with your arrow keys, because why not) ${axeEmoji}`
-        rewardsList.appendChild(reward2);
+        const rewardText = document.createElement('p');
+        rewardText.textContent = `Earned axe! (You can rotate the axe with your arrow keys, because why not) ${axeEmoji}`
+        rewardsList.appendChild(rewardText);
     }) 
+}
+
+function rotateAxe(e) {
+    const axeNode = document.getElementById('axe')
+
+    if (e.key === 'ArrowLeft') {
+        axeRotation-=45
+    } else if (e.key === 'ArrowRight') {
+        axeRotation+=45
+    }
+
+    axeNode.style.transform = `rotate(${axeRotation}deg)`
 }
 
 function addShield() {
@@ -253,9 +283,10 @@ function addShield() {
         shieldNode.style.top = '25px'
         shieldNode.style.fontSize = '40px'
         stickFigureContainer.appendChild(shieldNode)
-        const reward3 = document.createElement('p');
-        reward3.textContent = `Earned shield! ${shieldEmoji}`
-        rewardsList.appendChild(reward3);
+
+        const rewardText = document.createElement('p');
+        rewardText.textContent = `Earned shield! ${shieldEmoji}`
+        rewardsList.appendChild(rewardText);
     })
 }
 
@@ -273,9 +304,9 @@ function addFollowers() {
         manNode.style.fontSize = '40px'
         stickFigureContainer.appendChild(manNode)
 
-        const reward4 = document.createElement('p')
-        reward4.textContent = `Earned followers! ${manEmoji}`
-        rewardsList.appendChild(reward4);
+        const rewardText = document.createElement('p')
+        rewardText.textContent = `Earned followers! ${manEmoji}`
+        rewardsList.appendChild(rewardText);
 
 
 })
@@ -296,44 +327,12 @@ function addFollowers() {
 
 
 
-//Removes form bar, adds comment to task.
-function addCommentToTask(e) {
-    e.preventDefault();
-
-    const comment = e.target.comment.value
-    const completedTask = e.target.parentNode  
-
-    e.target.remove()
-
-    const commentNode = document.createElement('ul')
-    const commentText = document.createElement('li')
-    commentNode.appendChild(commentText)
-
-    commentText.textContent = `Comment: ${comment}`
-    
-    completedTask.appendChild(commentNode)
-
-    
-
-    
-    
-
-  }  
-
-function createHideButton() {
-    const hideButton = document.createElement('button')
-    hideButton.textContent = 'Hide'
-
-    completedTasksContainer.parentNode.insertBefore(hideButton, completedTasksContainer)
-
-    return hideButton
-  }
 
 
 
-//   function hideLog() {
-//     completedTasksContainer.style.display = 'none'
-// }
+
+
+
 
     
 
